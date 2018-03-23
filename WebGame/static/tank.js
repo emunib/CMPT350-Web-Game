c505e4b0321e810f8b038905f64c6968f40b2787
Acpeters <austin.clarke.peters@gmnail.com>
2018-03-23 16:34:58 -0600

    Took Tank controls out of game.js

---
+++ WebGame/static/tank.js
@@ -0,0 +1,26 @@
+let movement = {
+    up: false,
+    down: false,
+    left: false,
+    right: false
+};
+document.addEventListener('keydown', (event)=> {
+    switch (event.keyCode) {
+        case 65: // A
+            movement.left = true;
+            break;
+        case 68: // D
+            movement.right = true;
+            break;
+    }
+});
+document.addEventListener('keyup', (event)=> {
+    switch (event.keyCode) {
+        case 65: // A
+            movement.left = false;
+            break;
+        case 68: // D
+            movement.right = false;
+            break;
+    }
+});
