c0de55cc5c7ee0ed89bce3f73cd680b538ae4dee
Ehtasham <ehm011@mail.usask.ca>
2018-03-30 19:30:02 -0600

    other half of ground commit

--- WebGame/server.js
+++ WebGame/server.js
@@ -9,22 +9,22 @@ let io = socketIO(server);
 app.set('port', 5000);
 app.use('/static', express.static(__dirname + '/static'));
 // Routing
-app.get('/', function(request, response) {
+app.get('/', function (request, response) {
     response.sendFile(path.join(__dirname, 'index.html'));
 });
 // Starts the server.
-server.listen(5000, function() {
+server.listen(5000, function () {
     console.log('Starting server on port 5000');
 });
 var players = {};
-io.on('connection', function(socket) {
-    socket.on('new player', function() {
+io.on('connection', function (socket) {
+    socket.on('new player', function () {
         players[socket.id] = {
             x: 300,
-            y: 300
+            y: 0
         };
     });
-    socket.on('movement', function(data) {
+    socket.on('movement', function (data) {
         var player = players[socket.id] || {};
         if (data.left) {
             player.x -= 5;
@@ -32,18 +32,110 @@ io.on('connection', function(socket) {
         if (data.right) {
             player.x += 5;
         }
-
-    });
-    socket.on('gravity',data=>{
-        var player = players[socket.id] || {};
-        var grav = data.gravityVal;
-        // player.y += grav;
-        if(player.y < data.ground-10){
-            player.y += grav;
+        if (player.x < 0) {
+            player.x = 0;
         }
+        else if (player.x >= 800) {
+            player.x = 800 - 1;
+        }
+        player.y = ground(player, getPoints())
     });
+    socket.on('gravity', data => {
 
+    });
 });
-setInterval(function() {
-    io.sockets.emit('state', players);
+
+setInterval(function () {
+    io.sockets.emit('state', players, getPoints());
 }, 1000 / 60);
+
+
+
+
+
+var Simple1DNoise = function() {
+    var MAX_VERTICES = 256;
+    var MAX_VERTICES_MASK = MAX_VERTICES -1;
+    var amplitude = 1;
+    var scale = 1;
+
+    var r = [];
+
+    for ( var i = 0; i < MAX_VERTICES; ++i ) {
+        r.push(Math.random());
+    }
+
+    var getVal = function( x ){
+        var scaledX = x * scale;
+        var xFloor = Math.floor(scaledX);
+        var t = scaledX - xFloor;
+        var tRemapSmoothstep = t * t * ( 3 - 2 * t );
+
+        /// Modulo using &
+        var xMin = xFloor & MAX_VERTICES_MASK;
+        var xMax = ( xMin + 1 ) & MAX_VERTICES_MASK;
+
+        var y = lerp( r[ xMin ], r[ xMax ], tRemapSmoothstep );
+
+        return y * amplitude;
+    };
+
+    /**
+     * Linear interpolation function.
+     * @param a The lower integer value
+     * @param b The upper integer value
+     * @param t The value between the two
+     * @returns {number}
+     */
+    var lerp = function(a, b, t ) {
+        return a * ( 1 - t ) + b * t;
+    };
+
+    // return the API
+    return {
+        getVal: getVal,
+        setAmplitude: function(newAmplitude) {
+            amplitude = newAmplitude;
+        },
+        setScale: function(newScale) {
+            scale = newScale;
+        }
+    };
+};
+
+
+
+
+
+
+
+const segments = 100;
+const amplitude = 800 / 2;
+const smoothness = 0.01;
+
+let generator = new Simple1DNoise();
+let points = [];
+
+function getPoints() {
+    if (points.length === 0) {
+        for (let i = 0; i <= segments; i++) {
+            let y = generator.getVal(i * 800 / segments * smoothness) * amplitude;
+            points.push({x: i * 800 / segments, y: 600 - y})
+        }
+    }
+    return points
+}
+
+
+function ground(point, points) {
+    if (!(Object.keys(point).length === 0 && point.constructor === Object)) {
+        let i = Math.floor(point.x / (800 / segments));
+        let p1 = points[i];
+        let p2 = points[i + 1];
+
+        let m = (p2.y - p1.y) / (p2.x - p1.x);
+        let b = p1.y - m * p1.x;
+        return m * point.x + b;
+    }
+}
+
--- WebGame/static/game.js
+++ WebGame/static/game.js
@@ -3,29 +3,38 @@ let socket = io();
 socket.emit('new player');
 
 var canvas = document.getElementById('canvas');
-canvas.width = 800;
-canvas.height = 600;
+canvas.width = Constants.WIDTH;
+canvas.height = Constants.HEIGHT;
 
 let gravity = {
     gravityVal : 5,
-    gravitySpeed : 0,
-    ground : canvas.height
-}
+    gravitySpeed : 0
+};
 
 setInterval(function() {
     socket.emit('movement', movement);
-    socket.emit('gravity',gravity);
-    socket.emit('grounded',grounded);
+    socket.emit('gravity', gravity);
 }, 1000 / 60);
 
 var context = canvas.getContext('2d');
-socket.on('state', function(players) {
-    context.clearRect(0, 0, 800, 600);
-    context.fillStyle = 'green';
-    for (var id in players) {
+socket.on('state', function(players, points) {
+    context.clearRect(0, 0, Constants.WIDTH, Constants.HEIGHT);
+
+    context.fillStyle = 'red';
+    for (let id in players) {
         var player = players[id];
         context.beginPath();
         context.arc(player.x, player.y, 10, 0, 2 * Math.PI);
         context.fill();
     }
+
+    context.fillStyle = 'black';
+    context.beginPath();
+    context.moveTo(0, Constants.HEIGHT);
+    points.forEach((el) => {
+        context.lineTo(el.x, el.y);
+    });
+    context.lineTo(Constants.WIDTH, Constants.HEIGHT);
+    context.fill();
+    context.closePath();
 });
