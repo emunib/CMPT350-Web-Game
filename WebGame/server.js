2b726888e7e90b8c5b5dcbd1915a350427503220
Ehtasham <ehm011@mail.usask.ca>
2018-04-04 00:12:54 -0600

    add turns and some buttons

--- WebGame/server.js
+++ WebGame/server.js
@@ -15,6 +15,7 @@ let Vector = require('victor');
 let constants = require('./shared/constants.js');
 let ground = require('./lib/ground.js');
 let randColor = require('randomcolor');
+let Stopwatch = require('timer-stopwatch-dev');
 
 let sessionStore = new SQLiteStore;
 passport.serializeUser((user, done) => {
@@ -42,6 +43,7 @@ const PORT = process.env.PORT || 5000;
 app.set('port', PORT);
 app.use('/static', express.static(__dirname + '/static'));
 app.use('/shared', express.static(__dirname + '/shared'));
+app.use('/res', express.static(__dirname + '/res'));
 app.use(cookieParser());
 app.use(session({
     store: sessionStore,
@@ -85,16 +87,20 @@ app.get('/auth/google/callback',
     })
 );
 
-app.get('/logout', function(req, res){
-    let ids = Object.keys(players).filter((id) => {return players[id].id === req.user.id});
+app.get('/logout', (req, res) => {
+    let ids = Object.keys(players).filter((id) => {
+        return players[id].id === req.user.id
+    });
     req.logout();
-    ids.forEach((id) => {delete players[id]});
+    ids.forEach((id) => {
+        delete players[id]
+    });
     res.redirect('/');
 });
 
-let players = {};
-let viewers = {};
-let bullets = [];
+app.get('/new', ensureAuthenticated, (req, res) => {
+    res.redirect('/play');
+});
 
 io.use(passportSocketIo.authorize({
     key: 'connect.sid',
@@ -103,13 +109,44 @@ io.use(passportSocketIo.authorize({
     passport: passport,
 }));
 
+let players = {};
+let activeSockets = [];
+let viewers = {};
+let bullets = [];
+let status = {
+    turn: 0,
+    message: '',
+    playing: false
+};
+
+let mainTimer = new Stopwatch(20000, {refreshRateMS: 10, almostDoneMS: 3000});
+mainTimer.onTime((time) => {
+    status.message =(Math.floor(time.ms / 100) / 10).toFixed(1);
+}).onDone(() => {
+    status.playing = true;
+    turnTimer.start();
+});
+
+let turnTimer = new Stopwatch(10000);
+turnTimer.onTime((time) => {
+    status.message = (Math.floor(time.ms / 100) / 10).toFixed(1);
+}).onDone(() => {
+    status.turn++;
+    turnTimer.reset();
+});
+
 io.on('connection', (socket) => {
-    if (Object.values(players).some((player) => {
+    if (status.playing || Object.values(players).length > 5 || Object.values(players).some((player) => {
         return player.id === socket.request.user.id
     })) {
-        viewers[socket.id] = {};
+        viewers[socket.id] = {
+            id: socket.request.user.id,
+            name: socket.request.user.displayName,
+        };
     } else {
         socket.on('new player', () => {
+            mainTimer.start();
+            activeSockets.push(socket.id);
             players[socket.id] = {
                 id: socket.request.user.id,
                 name: socket.request.user.displayName,
@@ -119,14 +156,15 @@ io.on('connection', (socket) => {
                     power: 20
                 },
                 color: randColor(),
-                ammo: 1,
+                ammo: 1
             };
         });
     }
 
     socket.on('disconnect', () => {
         if (players.hasOwnProperty(socket.id)) {
-            players[socket.id] = {id: socket.request.user.id};
+            activeSockets.splice(activeSockets.indexOf(socket.id), 1);
+            delete players[socket.id];
         } else {
             delete viewers[socket.id];
         }
@@ -135,68 +173,75 @@ io.on('connection', (socket) => {
     socket.on('input', (data) => {
         let player = players[socket.id] || {};
 
-        if (Object.keys(player).length !== 0) {
-            let p = ground.getPoint(player.pos.x);
-            if (data.left) {
-                player.pos.x -= p.eqn.speed - p.eqn.m / 4;
-            }
-            if (data.right) {
-                player.pos.x += p.eqn.speed + p.eqn.m / 4;
-            }
-            if (player.pos.x < 0) {
-                player.pos.x = 0;
-            } else if (player.pos.x >= constants.WIDTH) {
-                player.pos.x = constants.WIDTH - 1;
-            }
-            player.pos.y = p.eqn.m * player.pos.x + p.eqn.b;
+        if (status.playing) {
+            if (activeSockets[status.turn % activeSockets.length] === socket.id) {
+                let p = ground.getPoint(player.pos.x);
+                if (data.left) {
+                    player.pos.x -= p.eqn.speed - p.eqn.m / 4;
+                }
+                if (data.right) {
+                    player.pos.x += p.eqn.speed + p.eqn.m / 4;
+                }
+                if (player.pos.x < 0) {
+                    player.pos.x = 0;
+                } else if (player.pos.x >= constants.WIDTH) {
+                    player.pos.x = constants.WIDTH - 1;
+                }
+                player.pos.y = p.eqn.m * player.pos.x + p.eqn.b;
 
-            if (data.aimleft) {
-                if (player.timer < 0) {
-                    player.aim.angle--;
+                if (data.aimleft) {
+                    if (player.timer < 0) {
+                        player.aim.angle--;
+                    } else {
+                        player.aim.angle -= 0.2;
+                        player.timer--;
+                    }
+                }
+
+                if (data.aimright) {
+                    if (player.timer < 0) {
+                        player.aim.angle++;
+                    } else {
+                        player.aim.angle += 0.2;
+                        player.timer--;
+                    }
+                }
+
+                if (!data.aimleft && !data.aimright) {
+                    player.timer = 30;
+                }
+
+                if (data.powerup) {
+                    player.aim.power++;
+                }
+
+                if (data.powerdown) {
+                    player.aim.power--;
+                }
+
+                if (player.aim.power > 100) {
+                    player.aim.power = 100;
+                } else if (player.aim.power < 0) {
+                    player.aim.power = 0;
+                }
+
+                if (data.fire) {
+                    if (player.ammo > 0) {
+                        let dir = new Vector(1, 0).rotateDeg(player.aim.angle);
+                        bullets.push({
+                            pos: player.pos.clone().add(dir.clone().multiplyScalar(10)),
+                            vel: dir.clone().multiplyScalar(player.aim.power / 10)
+                        });
+                        player.ammo--;
+                    }
                 } else {
-                    player.aim.angle -= 0.2;
-                    player.timer--;
+                    player.ammo = 1;
                 }
             }
-
-            if (data.aimright) {
-                if (player.timer < 0) {
-                    player.aim.angle++;
-                } else {
-                    player.aim.angle += 0.2;
-                    player.timer--;
-                }
-            }
-
-            if (!data.aimleft && !data.aimright) {
-                player.timer = 30;
-            }
-
-            if (data.powerup) {
-                player.aim.power++;
-            }
-
-            if (data.powerdown) {
-                player.aim.power--;
-            }
-
-            if (player.aim.power > 100) {
-                player.aim.power = 100;
-            } else if (player.aim.power < 0) {
-                player.aim.power = 0;
-            }
-
-            if (data.fire) {
-                if (player.ammo > 0) {
-                    let dir = new Vector(1, 0).rotateDeg(player.aim.angle);
-                    bullets.push({
-                        pos: player.pos.clone().add(dir.clone().multiplyScalar(10)),
-                        vel: dir.clone().multiplyScalar(player.aim.power / 10)
-                    });
-                    player.ammo--;
-                }
-            } else {
-                player.ammo = 1;
+        } else {
+            if (Object.keys(player).length > 2) {
+                let p = ground.getPoint(player.pos.x);
+                player.pos.y = p.eqn.m * player.pos.x + p.eqn.b;
             }
         }
     });
@@ -208,12 +253,16 @@ setInterval(() => {
         bullets[i].pos.add(bullets[i].vel);
 
         let hits = Object.keys(players).filter((id) => {
-            return (Object.keys(players[id]).length !== 0) && bullets[i].pos.distanceSq(players[id].pos) < 125;
+            return (Object.keys(players[id]).length > 2) && bullets[i].pos.distanceSq(players[id].pos) < 125;
         });
         if (hits.length > 0) {
             bullets.splice(i, 1);
             hits.forEach((id) => {
-                players[id] = {};
+                activeSockets.splice(activeSockets.indexOf(id), 1);
+                players[id] = {
+                    id: players[id].id,
+                    name: players[id].name
+                };
             })
         } else {
             if (bullets[i].pos.x < 0 || bullets[i].pos.x >= constants.WIDTH) {
@@ -227,8 +276,14 @@ setInterval(() => {
         }
     }
 
-    io.sockets.emit('state',
-        Object.keys(players).map((id) => {
+    if (status.playing && !turnTimer.isRunning()) {
+        turnTimer.start();
+    }
+
+    io.sockets.emit('state', status.message,
+        Object.keys(players).filter((id) => {
+            return Object.keys(players[id]).length > 2
+        }).map((id) => {
             return {
                 name: players[id].name,
                 pos: players[id].pos,
